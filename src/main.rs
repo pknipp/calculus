83dba25e363d21b0523d85cc3bf7ac4593294213
Peter Knipp <peteraknipp@gmail.com>
2022-02-03 08:22:57 -0500

    First commit.

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+  "This is my Rocket Demo app"
+}
+
+#[get("/about")]
+fn about() -> &'static str {
+  "This is the about page of my Rocket Demo app"
+}
+
+fn main() {
+  rocket::ignite().mount("/", routes![index, about]).launch();
+}
