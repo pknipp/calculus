a3550aa6366ce1517c04f28b779598254a9f1253
Peter Knipp <peteraknipp@gmail.com>
2022-02-04 23:02:32 -0500

    Implement 2 routes: index and evaluate.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Developed from https://dev.to/xinnks/deploy-a-rust-website-on-heroku-1l45
+https://docs.rs/eval/latest/eval/: allowed operations: +, -, *, /
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 use rocket::http::RawStr;
+use evalexpr::*;
 
 #[get("/")]
 fn index() -> &'static str {
-  "Welcome to Pete K's Rocket/Rust app."
+  "Welcome to Pete K's calculator app.\n In the url bar type any expression using +, -, *, or d (to represent division), and parentheses."
 }
 
-#[get("/hello/<name>")]
-fn hello(name: &RawStr) -> String {
-    format!("Welcome, {}!", name)
-}
-
-#[get("/about")]
-fn about() -> &'static str {
-  "This is the about page of my Rocket/Rust app"
+#[get("/<input_str>")]
+fn evaluate(input_str: &RawStr) -> String {
+  let math_str = str::replace(input_str, "d", "/");
+  match eval(&math_str.to_string()) {
+    Ok(val) => format!("{}", val),
+    Err(_) => String::from("String cannot be parsed."),
+  }
 }
 
 fn main() {
-  rocket::ignite().mount("/", routes![index, about, hello]).launch();
+  rocket::ignite().mount("/", routes![index, evaluate]).launch();
 }
