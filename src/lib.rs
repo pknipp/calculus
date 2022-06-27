9854c961f613db221e4ec854fab5ad09bb5d4b49
Peter Knipp <peteraknipp@gmail.com>
2022-06-26 23:30:10 -0400

    Tighten code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,66 +1,26 @@
 pub const INSTRUCTIONS: &str = "WELCOME TO MY CALCULUS APP";
 
-struct Link<'a> {
-	url: &'a str,
-	inner: &'a str,
-	outer: &'a str,
-}
-
-const LINKS: [Link; 8] = [
-	Link{
-		url: "/math",
-		inner: "back to",
-		outer: " math APIs page",
-	},
-	Link{
-		url: "",
-		inner: "back to",
-		outer: " calculus page",
-	},
-	Link{
-		url: "/differentiation",
-		inner: "differentiation",
-		outer: "",
-	},
-	Link{
-		url: "/integration",
-		inner: "integration",
-		outer: "",
-	},
-	Link{
-		url: "/root-finding",
-		inner: "root-finding",
-		outer: "",
-	},
-	Link{
-		url: "/max-finding",
-		inner: "max-finding",
-		outer: "",
-	},
-	Link{
-		url: "/ode",
-		inner: "1st order",
-		outer: " differential equations",
-	},
-	Link{
-		url: "/ode2",
-		inner: "2nd order",
-		outer: " differential equations",
-	},
+const LINKS: [[&str; 3]; 8] = [
+	["/math", "back to", " math APIs page"],
+	["", "back to", " calculus page"],
+	["/differentiation", "differentiation", ""],
+	["/integration", "integration", ""],
+	["/root-finding", "root-finding", ""],
+	["/max-finding", "max-finding", ""],
+	["/ode", "1st order", " differential equations"],
+	["/ode2", "2nd order", " differential equations"],
 ];
 
 pub fn general_page() -> String {format!("<p align=center>{}</p><p align=center>{}</p>", INSTRUCTIONS, links(1))}
 
 pub fn links(n: i32) -> String {
 	let mut links = "".to_string();
-	for i in 0..8 {
-		if i != n {
+	for j in 0..8 {
+		let i = j as usize;
+		if i != n as usize {
 			links = format!("{}
 				<a href='https://basic-calculus.herokuapp.com{}'>{}</a>{}<br>",
-				links,
-			  	LINKS[i as usize].url,
-			  	LINKS[i as usize].inner,
-			  	LINKS[i as usize].outer,
+				links, LINKS[i][0], LINKS[i][1], LINKS[i][2]
 			);
 		}
 	}
